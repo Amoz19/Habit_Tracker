8201923553da0816b1cfa89718f28c1c06df1c03
Amoz19 <aungmon10co.com>
2023-12-15 02:53:02 +0630

    adjust:ForProduction

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,12 +12,31 @@ const dbConnect = process.env.DB_URL;
 const secret = process.env.SECRET_KEY;
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 app.use(express.json());
 
 const mongoDbStore = new MongoDbStore({
@@ -35,13 +54,13 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 3,
       sameSite: false,
-      secure: false,
+      secure: process.env.NODE_ENV === "production",
     },
   })
 );
 
 app.use("/api/v1", calendarRoute);
-app.use("/api/user", userRoute);
+app.use("/api/user", allowCors(userRoute));
 
 mongoose
   .connect(dbConnect)
