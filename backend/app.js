e833e4e81c39d9bccccd8876a0019deac2ff21f3
Amoz19 <aungmon10co.com>
2023-12-15 03:42:48 +0630

    fix:Error

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const calendarRoute = require("../routes/calendarRoute");
-const userRoute = require("../routes/userRoute");
+const calendarRoute = require("./routes/calendarRoute");
+const userRoute = require("./routes/userRoute");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDbStore = require("connect-mongodb-session")(session);
@@ -15,28 +15,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
-
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
 
 app.use(express.json());
 
@@ -60,8 +42,8 @@ app.use(
   })
 );
 
-app.use("/api/v1", allowCors(calendarRoute));
-app.use("/api/user", allowCors(userRoute));
+app.use("/api/v1", calendarRoute);
+app.use("/api/user", userRoute);
 
 app.get("/", (req, res) => {
   return res.send("Hello");
