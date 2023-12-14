7ac10ec4c2e31f18a103a24c6ba7e8d6f6f933e4
Amoz19 <aungmon10co.com>
2023-12-15 03:00:04 +0630

    add:VercelConfig

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,16 +12,17 @@ const dbConnect = process.env.DB_URL;
 const secret = process.env.SECRET_KEY;
 
 const app = express();
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
