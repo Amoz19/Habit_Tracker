163ec275e1fed283626c94d0b2385f5efab46b0c
Amoz19 <aungmon10co.com>
2023-12-15 20:11:39 +0630

    fix:cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
