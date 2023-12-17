a3d0df265b5d3b0f9dde003b3bfc9aad734c1961
Amoz19 <aungmon10co.com>
2023-12-17 10:54:25 +0630

    add:Style

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuthFunction } from "../../hook/useAuthForm.js";
 import { useUser } from "../../context/AuthContext.jsx";
-// import { QueryClient } from "react-query";
+import styles from "./Auth.module.css";
 
 const Login = ({ handleOpen }) => {
   // const queryClient = QueryClient();
@@ -35,51 +35,51 @@ const Login = ({ handleOpen }) => {
     <div className="bg-gradient-to-b from-indigo-200 to-indigo-300 flex flex-col justify-center h-screen items-center">
       <form
         onSubmit={handleSubmit(onHandleSubmit)}
-        className="bg-slate-100 w-2/6 p-6 rounded"
+        className="bg-slate-100 w-[90%] md:w-3/6 p-6 rounded"
       >
-        <h1 className="text-2xl mb-4">Login</h1>
+        <h1 className="text-2xl font-black mb-4 text-blue-900">Login</h1>
         <div className="flex flex-col">
-          <label htmlFor="username" className="mb-2 text-sm font-medium">
+          <label htmlFor="username" className="mb-2 text-slate-700">
             Username
           </label>
           <input
             type="text"
             {...register("username", { required: true })}
             id="username"
-            className="border border-gray-300 py-1 px-3 focus:outline outline-blue-100 rounded"
+            className={`border border-gray-300 py-1 px-3 focus:outline outline-blue-100 rounded text-slate-700 ${styles.placeholder}`}
             placeholder="Enter username"
           />
           {errors.username && (
-            <p className="text-red-900">Username is required</p>
+            <p className="text-red-600 mt-2 px-1">Username is required</p>
           )}
         </div>
         <div className="my-4 flex flex-col">
-          <label htmlFor="password" className="mb-2 text-sm font-medium">
+          <label htmlFor="password" className="mb-2 text-slate-700">
             Password
           </label>
           <input
             type="password"
             {...register("password", { required: true })}
             id="password"
-            className="py-1 px-3 focus:outline outline-blue-100 rounded"
+            className={`border border-gray-300 py-1 px-3 focus:outline outline-blue-100 rounded text-slate-700 ${styles.placeholder}`}
             placeholder="Enter password"
           />
           {errors.password && (
-            <p className="text-red-900">Password is required</p>
+            <p className="text-red-600 mt-2 px-1">Password is required</p>
           )}
         </div>
 
         <input
           type="submit"
-          className="bg-blue-900 text-white px-4 py-1 rounded-sm w-full mb-6 disabled:opacity-40"
-          value="login"
+          className="bg-blue-900 text-white px-4 py-1 rounded mb-6 disabled:opacity-40 text-sm mr-2"
+          value="Login"
           disabled={isLoading}
         />
         {isError && (
           <p className="mb-3 text-center text-red-600">{error.message}</p>
         )}
         <button
-          className="bg-green-500 w-full rounded text-white "
+          className="px-3 py-1 border border-gray-300 rounded text-sm text-blue-800"
           onClick={handleOpen}
         >
           Create new accout
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { Navigate } from "react-router-dom";
 import { useAuthFunction } from "../../hook/useAuthForm.js";
 import Loading from "../Loading.jsx";
+import styles from "./Auth.module.css";
 
 const Signup = ({ handleClose }) => {
   const {
@@ -20,60 +21,77 @@ const Signup = ({ handleClose }) => {
   };
 
   return (
-    <div className="bg-gradient-to-b from-indigo-200 to-indigo-300 flex flex-col justify-center items-center flex-1">
+    <div className="bg-gradient-to-b from-indigo-200 to-indigo-300 flex flex-col justify-center h-screen items-center">
       <form
         onSubmit={handleSubmit(onHandleSubmit)}
-        className="bg-slate-100 w-fit p-6 rounded"
+        className="bg-slate-100 w-[90%] md:w-3/6 p-6 rounded mx-8"
       >
+        {isError && (
+          <p className="mt-3 text-center text-red-600 bg-white rounded py-1 shadow">
+            {error.message}
+          </p>
+        )}
         <div
           onClick={handleClose}
           className=" flex justify-between items-center mb-3"
         >
-          {isSuccess && <p className="text-green-600">Success</p>}
+          {isSuccess && <p className="text-blue-600">Success✓</p>}
           <div className="w-fit py-1  text-white">
             {
               <div onClick={handleClose}>
                 {isSuccess ? (
-                  <p className="bg-green-600 px-3 py-1 rounded">Login</p>
+                  <p className="text-blue-600 px-3 py-1 rounded border border-gray-300">
+                    Login
+                  </p>
                 ) : (
-                  <p className="bg-red-600 px-3 py-1 rounded">X</p>
+                  <p className="px-3 py-1 rounded text-red-600 border border-gray-300">
+                    X
+                  </p>
                 )}
               </div>
             }
           </div>
         </div>
-        <div>
+        <h1 className="text-l md:text-xl font-black text-blue-900 mb-4 ">
+          Create New Account
+        </h1>
+        <div className="my-4 flex flex-col">
+          <label htmlFor="username" className="mb-2 text-slate-700">
+            Username
+          </label>
           <input
             type="text"
             {...register("username", { required: true })}
-            className="bg-blue-100 py-1 px-3 focus:outline outline-blue-100 rounded"
+            className={`border border-gray-300 py-1 px-3 focus:outline outline-blue-100 rounded  ::placeholder:text-sm text-slate-700 ${styles.placeholder}`}
+            id="username"
             placeholder="Enter username"
           />
           {errors.username && (
-            <p className="text-red-900">Username is required</p>
+            <p className="text-red-600 mt-2 px-1">Username is required</p>
           )}
         </div>
-        <div className="py-6">
+        <div className="my-4 flex flex-col">
+          <label htmlFor="password" className="mb-2 text-slate-700">
+            Password
+          </label>
           <input
             type="password"
             {...register("password", { required: true })}
-            className="bg-blue-100 py-1 px-3 focus:outline outline-blue-100 rounded"
+            id="password"
+            className={`border border-gray-300 py-1 px-3 focus:outline outline-blue-100 rounded ${styles.placeholder} text-slate-800`}
             placeholder="Enter password"
           />
           {errors.password && (
-            <p className="text-red-900">Password is required</p>
+            <p className="text-red-600 mt-2 px-1">Password is required</p>
           )}
         </div>
 
         <input
           type="submit"
-          className="bg-blue-900 text-white px-4 py-1 rounded-sm w-full disabled:opacity-40"
+          className="bg-blue-900 text-white px-4 py-1 rounded disabled:opacity-40"
           value="signup"
           disabled={isLoading}
         />
-        {isError && (
-          <p className="mt-3 text-center text-red-600">{error.message}</p>
-        )}
       </form>
     </div>
   );
