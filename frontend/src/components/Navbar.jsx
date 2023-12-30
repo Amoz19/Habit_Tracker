455d40be72cb846e7a4adf1851b53b5c6f54fb98
Amoz19 <aungmon10co.com>
2023-12-30 19:40:04 +0630

    modify:Style

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,47 +1,16 @@
 import logo from "../assets/animatelogo.gif";
-import { useUser } from "../context/AuthContext";
-import { useLogoutFunction } from "../hook/useAuthForm";
-import { useNavigate } from "react-router-dom";
+
 import Theme from "./Theme";
+import User from "./User";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const { mutate: userLogout } = useLogoutFunction();
-  const { user, logout } = useUser();
-
-  const handleLogout = () => {
-    userLogout("logout", {
-      onSuccess: () => {
-        logout();
-        navigate("/auth");
-      },
-    });
-  };
-
   return (
     <div className="bg-[#e6e6e6] dark:bg-black shadow-sm px-8 md:px-32 py-6 sticky top-0 flex justify-between">
       <div className="flex items-center">
         <img src={logo} alt="logo" className="w-24" />
         <Theme />
       </div>
-
-      <div className="flex items-center">
-        {user && (
-          <div className="flex items-center">
-            {user && (
-              <p className="text-sm text-indigo-700 dark:text-indigo-300 dark:font-bold">
-                Hi, {user.username}
-              </p>
-            )}
-            <button
-              onClick={handleLogout}
-              className="bg-indigo-500  text-white p-2 rounded ml-4 text-sm"
-            >
-              Logout
-            </button>
-          </div>
-        )}
-      </div>
+      <User />
     </div>
   );
 };
---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import {
+  MaterialSymbolsLightAccountCircleOutline,
+  IonMdLogOut,
+} from "../../util/icon";
+import { useUser } from "../context/AuthContext";
+import { useLogoutFunction } from "../hook/useAuthForm";
+import { useNavigate } from "react-router-dom";
+
+const User = () => {
+  const [isActive, setIsActive] = useState(false);
+  const navigate = useNavigate();
+  const { mutate: userLogout } = useLogoutFunction();
+  const { user, logout } = useUser();
+
+  const handleLogout = () => {
+    userLogout("logout", {
+      onSuccess: () => {
+        logout();
+        navigate("/auth");
+      },
+    });
+  };
+
+  return (
+    <div className="flex relative">
+      <MaterialSymbolsLightAccountCircleOutline
+        onClick={() => setIsActive(!isActive)}
+      />
+      {isActive && (
+        <div className=" bg-white p-2 rounded absolute top-10 right-2  shadow transition-all w-24 ">
+          {user && (
+            <div className="flex flex-col items-center">
+              {user && (
+                <p className="text-sm text-indigo-700  dark:font-bold">
+                  Hi, {user.username}
+                </p>
+              )}
+              <button
+                onClick={handleLogout}
+                className="bg-indigo-500  text-white px-4 py-2 rounded ml-4 text-sm mt-2"
+              >
+                <span>
+                  <IonMdLogOut />
+                </span>
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default User;
